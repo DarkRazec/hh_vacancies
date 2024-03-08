caa4ea3c5c8bf48595e20ed40d1555325c75782d
DarkRazec <darkrazec@yahoo.com>
2024-03-08 21:54:14 +0300

    deleted exp field in API's get_vacancies method

--- src/modules/HeadHunterAPI.py
+++ src/modules/HeadHunterAPI.py
@@ -10,18 +10,16 @@ class HeadHunterAPI(APIVacancy):
     def __init__(self):
         self.__url = 'https://api.hh.ru/'
 
-    def get_vacancies(self, search_query: str, search_num: int = 0, page: int = 0, exp: str = None) -> list[dict]:
+    def get_vacancies(self, search_query: str, search_num: int = 0, page: int = 0) -> list[dict]:
         """
         Обращается к API сервису и возвращает список вакансий
         :param search_query: Запрос пользователя
         :param search_num: Количество элементов выдачи (макс. 100)
         :param page: Номер страницы
-        :param exp: Опыт работы. Принимает значения: "noExperience", "between1And3", "between3And6", "moreThan6" TODO
         :return: Список словарей
         """
         params = {
             'text': search_query,
-            'experience': exp,
             'order_by': 'relevance',
             'per_page': search_num if search_num > 0 else 10,
             'page': page if page >= 0 else 0
