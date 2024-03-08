63985325de2cf4f708da5e5716bd991292c1e79e
DarkRazec <darkrazec@yahoo.com>
2024-03-08 21:38:33 +0300

    made Salary class out of Vacancy class

--- src/modules/Vacancy.py
+++ src/modules/Vacancy.py
@@ -1,7 +1,7 @@
-from src.get_currency_rate import get_currency_rate
+from src.modules.Salary import Salary
 
 
-class Vacancy:
+class Vacancy(Salary):
     """Класс для абстракции 'Вакансия'"""
     name: str
     url: str
@@ -15,16 +15,16 @@ class Vacancy:
     __slots__ = ('__name', '__url', '__from', '__to', '__currency', '__area', '__company', '__schedule', '__exp')
 
     def __init__(self, name: str, url: str, salary: tuple, desc: tuple):
+        super().__init__(salary)
         self.__name = name
         self.__url = url
-        self.__from, self.__to, self.__currency = salary if salary else (0, 0, 'RUR')
         self.__area, self.__company,  self.__schedule, self.__exp = desc if desc else (None, None, None, None)
 
     def __str__(self):
-        if self.__from:
-            if self.__to:
-                return f"Вакансия {self.__name} с заработной платой от {self.__from} до {self.__to} {self.__currency} {self.__url}"
-            return f"Вакансия {self.__name} с заработной платой {self.__from} {self.__currency} {self.__url}"
+        if self.get_from():
+            if self.get_to():
+                return f"Вакансия {self.__name} с заработной платой от {self.get_from()} до {self.get_to()} {self.currency} {self.__url}"
+            return f"Вакансия {self.__name} с заработной платой {self.get_from()} {self.currency} {self.__url}"
         return f"Вакансия {self.__name} c неуказанной заработной платой {self.__url}"
 
     # Геттеры и Property
@@ -35,10 +35,6 @@ class Vacancy:
     @property
     def url(self):
         return self.__url
-
-    @property
-    def salary(self) -> tuple[int, int, str | None]:
-        return self.__from, self.__to, self.__currency
 
     @property
     def area(self):
@@ -63,30 +59,14 @@ class Vacancy:
         return "без опыта"
 
     @property
-    def currency(self):
-        return self.__currency
-
-    @property
     def desc(self):
         return (f"Компания {self.__company} предлагает данную вакансию в городе {self.area}. "
                 f"График работы: {self.schedule}. Требуемый опыт: {self.exp}")
 
-    def get_from(self):
-        if self.__from:
-            return self.__from
-        return 0
-
-    def get_to(self):
-        if self.__to:
-            return self.__to
-        return 0
-
     # Сравнение вакансий
     def __lt__(self, other):
         try:
-            this_from, this_to = self.salary_to_rub()
-            other_from, other_to = other.salary_to_rub()
-            if self.salary_median(this_from, this_to) < other.salary_median(other_from, other_to):
+            if self.salary_median() < other.salary_median():
                 return True
             return False
         except AttributeError:
@@ -94,9 +74,7 @@ class Vacancy:
 
     def __le__(self, other):
         try:
-            this_from, this_to = self.salary_to_rub()
-            other_from, other_to = other.salary_to_rub()
-            if self.salary_median(this_from, this_to) <= other.salary_median(other_from, other_to):
+            if self.salary_median() <= other.salary_median():
                 return True
             return False
         except AttributeError:
@@ -104,9 +82,7 @@ class Vacancy:
 
     def __eq__(self, other):
         try:
-            this_from, this_to = self.salary_to_rub()
-            other_from, other_to = other.salary_to_rub()
-            if self.salary_median(this_from, this_to) == other.salary_median(other_from, other_to):
+            if self.salary_median() == other.salary_median():
                 return True
             return False
         except AttributeError:
@@ -114,9 +90,7 @@ class Vacancy:
 
     def __ne__(self, other):
         try:
-            this_from, this_to = self.salary_to_rub()
-            other_from, other_to = other.salary_to_rub()
-            if self.salary_median(this_from, this_to) != other.salary_median(other_from, other_to):
+            if self.salary_median() != other.salary_median():
                 return True
             return False
         except AttributeError:
@@ -124,9 +98,7 @@ class Vacancy:
 
     def __gt__(self, other):
         try:
-            this_from, this_to = self.salary_to_rub()
-            other_from, other_to = other.salary_to_rub()
-            if self.salary_median(this_from, this_to) > other.salary_median(other_from, other_to):
+            if self.salary_median() > other.salary_median():
                 return True
             return False
         except AttributeError:
@@ -134,25 +106,10 @@ class Vacancy:
 
     def __ge__(self, other):
         try:
-            this_from, this_to = self.salary_to_rub()
-            other_from, other_to = other.salary_to_rub()
-            if self.salary_median(this_from, this_to) >= other.salary_median(other_from, other_to):
+            if self.salary_median() >= other.salary_median():
                 return True
             return False
         except AttributeError:
             raise TypeError("Переданный аргумент не является объектом класса Vacancy")
 
-    def salary_to_rub(self) -> tuple[int, int] | tuple[float, float]:
-        """Возвращает значения зарплаты приведенные к рублю"""
-        if self.__currency != 'RUR':
-            curr_rate = get_currency_rate(self.__currency)
-            new_from, new_to = [curr_rate * i for i in (self.get_from(), self.get_to())]
-            return new_from, new_to
-        return self.get_from(), self.get_to()
 
-    @staticmethod
-    def salary_median(salary_from: int | float, salary_to: int | float) -> int:
-        """Возвращает среднюю зарплату, если в аргументы передан диапазон зарплаты"""
-        if salary_to:
-            return round((salary_from + salary_to) / 2)
-        return salary_from
