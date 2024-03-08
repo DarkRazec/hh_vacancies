798a03b16a9f85fe56de281d9b6fca3056443129
DarkRazec <darkrazec@yahoo.com>
2024-03-08 03:09:32 +0300

    add typing and docstring to json_to_vacancies

--- src/json_to_vacancies.py
+++ src/json_to_vacancies.py
@@ -1,6 +1,11 @@
 from src.modules.Vacancy import Vacancy
 
 
-def json_to_vacancies(raw_vacancies):
+def json_to_vacancies(raw_vacancies) -> list[Vacancy]:
+    """
+    Возвращает список объектов класса Vacancy, сформированных из JSON формата
+    :param raw_vacancies: список вакансий в формате JSON
+    :return: Список объектов Vacancy
+    """
     return [Vacancy(vacancy["name"], vacancy["url"], vacancy["salary"].values(),
                     vacancy["desc"].values()) for vacancy in raw_vacancies]
