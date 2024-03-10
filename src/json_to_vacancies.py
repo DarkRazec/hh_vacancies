5e4501d72571b8de14ab4467cd8552958c2d6cac
DarkRazec <darkrazec@yahoo.com>
2024-03-10 09:29:48 +0300

    optimized Salary's parameters validation

--- src/json_to_vacancies.py
+++ src/json_to_vacancies.py
@@ -7,5 +7,5 @@ def json_to_vacancies(raw_vacancies: list[dict]) -> list[Vacancy]:
     :param raw_vacancies: список вакансий в формате JSON
     :return: Список объектов Vacancy
     """
-    return [Vacancy(vacancy["name"], vacancy["url"], vacancy["salary"].values(),
+    return [Vacancy(vacancy["name"], vacancy["url"], tuple(vacancy["salary"].values()),
                     vacancy["desc"].values()) for vacancy in raw_vacancies]
